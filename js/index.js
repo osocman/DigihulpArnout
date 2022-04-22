b25cb0abd191e79557bbbf725e2e218db3f2e096
osocman <aernoutgroenendijk@gmail.com>
2022-04-22 12:13:56 +0200

    Minimal Viable Product Version

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 const burgerBtn = document.querySelector(".burger")
 const navList = document.querySelector('.nav__list')
 
+function hideSideMenu() {
+    navList.classList.add("hidden")
+}
+
 burgerBtn.addEventListener('click', function() {
     if (navList.classList.contains("hidden")){
         navList.classList.remove("hidden")
     } else {
-        navList.classList.add("hidden")
+        hideSideMenu()
     }
 })
+
+document.querySelectorAll('.nav__list__link').forEach(item => {
+    item.addEventListener('click', event => {
+      if (!navList.classList.contains("hidden")) {
+          hideSideMenu()
+      }
+    })
+  })
